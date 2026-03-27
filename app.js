fea7df3b031a3b4a0c511f8403c49e32da2a158e
Anaid-Ariwany <anaidariwany@gmail.com>
2026-03-27 18:34:53 +0300

    2,3. Created the `operate` function

--- app.js
+++ app.js
@@ -21,3 +21,25 @@ function divide(num1, num2) {
     }
     return num1 / num2;
 }
+
+/* calculator operations */
+//create 3 variables to hold the numbers and operator
+let number1 = 0;
+let number2 = 0;
+let operator = "";
+
+//function `operate` that takes an operator and 2 numbers and calls the correct function based on the operator
+function operate(num1, operator, num2) {
+    switch (operator) {
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2);
+        case "*":
+            return multiply(num1, num2);
+        case "/":
+            return divide(num1, num2);
+        default:
+            return "Error: Invalid operator.";
+    }
+}
